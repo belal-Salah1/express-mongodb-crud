f8ddaea6d8e3ebce9012bfc27433f29b0d5c455c
belal-Salah1 <belal.salah259@gmail.com>
2025-07-12 05:19:10 +0300

    adding middleware

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,72 +1,74 @@
 const Course = require("../models/model.schema");
 const {validationResult} = require('express-validator');
 const httpStatusText = require('../utilites/statusCodeText');
+const asyncWrapper = require('../middlewares/asyncWrapper');
+const appError = require('../utilites/appError')
 
-const getAllCourses = async (req, res) => {
+const getAllCourses = asyncWrapper( async (req, res) => {
   const query = req.query
   const limit = query.limit || 2;
   const page = query.page  || 1;
   const skip = (page - 1) * limit;
-  try {
+  
     // const courses = await Course.find({},{"__v":false }).limit(limit).skip(skip) ;
      const courses = await Course.find({},{"__v":false }) ;
     if (!courses) {
       res.status(404).json({status:httpStatusText.FAIL , data : {courses:null}});
     }
     res.status(200).json({status:httpStatusText.SUCCESS , data : {Courses : courses}});
-  } catch (err) {
-    return res.status(400).json({status:httpStatusText.ERROR , message :err.message});
-  }
-};
+  
+}
+)
 
-const getSingleCourse = async (req, res) => {
-  try {
-    const course = await Course.findById(req.params.id);
-    if (!course) {
-      res.status(400).json({status:httpStatusText.FAIL , data : {courses:null}});
+const getSingleCourse = asyncWrapper(
+async (req, res,next) => {
+  
+  const course = await Course.findById(req.params.id);
+  if (!course) {
+      const error = appError.create("course not found", 404 , httpStatusText.FAIL);
+      return next(error)
     }
-    res.status(200).json({status:httpStatusText.SUCCESS , data : {Course : course}});
-  } catch (err) {
-    return res.status(400).json({status:httpStatusText.ERROR , message :err.message});
-  }
-};
+    return res.status(200).json({status:httpStatusText.SUCCESS , data : {Course : course}});
+   
+ 
+}
+)
 
-const addCourse = async (req, res) => {
-  try {
+const addCourse = asyncWrapper( async(req, res , next) => {
+  
     const NewCourse = await new Course(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({status:httpStatusText.FAIL , errors:errors.array()});
-      return;
+      const error = appError.create(errors.array(),400, httpStatusText.FAIL )
+      return next(error)      
     }
     NewCourse.save();
     res.status(201).json({status:httpStatusText.SUCCESS , data : {Course : NewCourse}});
-  } catch (err) {
-    return res.status(400).json({status:httpStatusText.ERROR , message :err.message});
-  }
-};
+   
+    
+  
+}
+)
 
-const updateCourse = async (req, res) => {
-  try {
+const updateCourse = asyncWrapper(async (req, res) => {
+  
     const course = await Course.findByIdAndUpdate(
       req.params.id,
       { $set: req.body },
       { new: true }
     );
     res.status(200).json({status:httpStatusText.SUCCESS , data : {Course : course}});
-  } catch (err) {
-    return res.status(400).json({status:httpStatusText.ERROR , message :err.message});
-  }
-};
+ 
+}
+)
 
-const deleteCourse = async (req, res) => {
-  try {
+const deleteCourse = asyncWrapper(async (req, res) => {
+  
     await Course.findByIdAndDelete(req.params.id);
     res.status(200).json({status:httpStatusText.SUCCESS , data : {msg: "course deleted successfully" }});
-  } catch (err) {
-    return res.status(400).json({status:httpStatusText.ERROR , message :err.message});
-  }
-};
+
+}
+)
 
 module.exports = {
   getAllCourses,
--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const app = express();
 const mongoose = require('mongoose');
 const router = require("./routes/courses.routes")
 const Url = process.env.MONGO_URL;
+const httpStatusText = require('./utilites/statusCodeText');
 
 
 mongoose.connect(Url).then(()=>{
     console.log("connected to mongodb succesfully")
 })
 app.use(express.json());
-app.use('/api/courses',router)
+app.use('/api/courses',router);
+app.use((error,req,res,next)=>{
+    const statusCode = error.statusCode
+    res.status(statusCode || 404  ).json({status:error.statusText|| httpStatusText.ERROR ,   message: (error.message || "error happend") ,code:error.statusCode|| 500 ,data : "null"})
+})
 
 
 app.listen(process.env.PORT || 3000, ()=>{
